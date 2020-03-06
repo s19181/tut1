67913304eed94bda12836893660e0ce6e3b6a3d5
s19181 <s19181@pjwstk.edu.pl>
2020-03-06 09:59:51 +0100

    Modified the project

--- Tutorial1Sol/Tutorial1/Program.cs
+++ Tutorial1Sol/Tutorial1/Program.cs
@@ -9,24 +9,50 @@ namespace Tutorial1
     {
         public static async Task Main(string[] args) //Task same as void, but used for async programming(faster)
         {
-            var websiteUrl = args[0];
+
+            //string websiteUrl = null;
+            //var x = websiteUrl ?? throw new ArgumentException("url cant be null"); (checking for null)
+
+
+            var websiteUrl = args.Length > 0 ? args[0] : throw new ArgumentNullException();
+          
             var httpClient = new HttpClient();
             var response =  await httpClient.GetAsync(websiteUrl);
-            if (response.IsSuccessStatusCode)
+            httpClient.Dispose();
+            try
             {
-                var htmlContent = await response.Content.ReadAsStringAsync();
-
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                var emailAddresses = regex.Matches(htmlContent);
-                foreach (var emailAddress in emailAddresses)
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine(emailAddress.ToString());
+                    var htmlContent = await response.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var emailAddresses = regex.Matches(htmlContent);
+
+                    if (emailAddresses.Count > 0)
+                    {
+                        foreach (var emailAddress in emailAddresses)
+                        {
+                            Console.WriteLine(emailAddress.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No email address found");
+                    }
                 }
             }
+            catch (Exception)
+            {
+                Console.WriteLine("Error while downloading the page");
+            }
+
+
+
+
             Console.ReadKey();
 
 
-        }
+            }
     }
 }
